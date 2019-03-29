99968ea2ae894023b70be3041577ffc20b3e679e
osiresb <osiresbideug@gmail.com>
2019-03-29 16:17:45 -0300

    Create Match_Template.py

---
+++ Match_Template.py
@@ -0,0 +1,55 @@
+#Template Matching 
+import numpy as np
+import cv2
+
+class match_Prod:
+
+    def mostra(product):
+        print(product , 'existe')
+
+
+    imagem = ['img0.png', 'img1.png', 'img2.png', 'img3', 'img4', 'img5.png',
+             'img6.png', 'img7.png', 'img8', 'img9', 'img10.png', 'img11.png',
+             'img12.png', 'img13', 'img14', 'img15.png', 'img16.png', 'img17.png', 'img18', 'img19', 'img20']
+    item = ['refrigerante.png','lanche15.png', 'lanche30.png']
+    
+
+    #Ler a imagen principal e template
+    img_rgb = cv2.imread(imagem[1])
+    template0 = cv2.imread(item[0],0)
+    template1 = cv2.imread(item[1],0)
+    template2 = cv2.imread(item[2],0)
+    #Converter 
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+    #largura e altura do template w,h
+    w, h = template1.shape[::-1] 
+    #verificar match (analise qual eh melhor metodo)
+    #res = cv2.matchTemplate(img_gray,template, cv2.TM_CCOEFF)
+    res = cv2.matchTemplate(img_gray,template0, cv2.TM_CCOEFF_NORMED)
+    res = cv2.matchTemplate(img_gray,template1, cv2.TM_CCOEFF_NORMED)
+    res = cv2.matchTemplate(img_gray,template2, cv2.TM_CCOEFF_NORMED)
+    #res = cv2.matchTemplate(img_gray,template, cv2.TM_SQDIFF)
+    #res = cv2.matchTemplate(img_gray,template, cv2.TM_SQDIFF_NORMED)
+
+
+    #Especificar um limite (threshold)
+    threshold = 0.7
+
+    #array numpy para area conscidente
+    loc = np.where( res >= threshold)
+    #desenhar retangulo para area encontrada
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), 1)
+   
+    print('Produtos encontrados \n')
+    for i in item:
+       # if(item[i] == 'refrigerante.png')
+        mostra(' {}'.format(i))
+
+    #Imagem final com area conscidente encontrado
+    cv2.imshow('Produto detectado: ',img_rgb)
+    #cv2.imwrite('refri_detectado',img_rgb)
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
